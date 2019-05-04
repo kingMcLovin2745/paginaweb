c445fbcdf72be281a015f42e7aebc91ffad7794f
kingMcLovin2745 <47837474+kingMcLovin2745@users.noreply.github.com>
2019-05-04 16:09:59 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+function Info() {
+    alert ('Website created by:\nJuan Carlos Martinez Toledo\nUniversidad de Guanajuato\nNUA:145206\nCorreo:jc.martineztoledo@ugto.mx\nCel:(+52) 456-139-6075');
+}
+
+function InfoT() {
+    alert ('House Targaryen is a noble family of Valyrian descent who once ruled the Seven Kingdoms of Westeros.\nThe Targaryen sigil is a three-headed dragon breathing flames, red on black.\nThe house words are "Fire and Blood"');
+}
+
+function InfoM() {
+    alert ('House Mormont of Bear Island is a noble house of the north, one of the principal families sworn to House Stark.\nTheir seat is at Bear Island, located in the Bay of Ice northwest of Winterfell.\nTheir blazon is a black bear over a green wood and their motto is "Here We Stand"');
+}
+
+function InfoG() {
+    alert ('House Greyjoy of Pyke is one of the Great Houses of Westeros.\nIt rules over the Iron Islands, a harsh and bleak collection of forbidding islands off the west coast of Westeros, from the Seastone Chair in the castle of Pyke on the island of the same name.\nThe head of the family is traditionally known as the Lord Reaper of Pyke.\nTheir sigil is a golden kraken on a black field, and their house motto is "We Do Not Sow."');
+}
+
+function InfoB() {
+    alert ('House Baratheon of Storms End is one of the Great Houses of Westeros, and is the principal house in the stormlands, which they rule as Lords Paramount of the Stormlands.\nTheir seat, Storms End, is an ancient castle raised by the Storm Kings from the now-extinct House Durrandon.\nThe Baratheon sigil is a crowned black stag on a field of gold.\nMembers of the family tend to be tall and powerfully built, with black hair and blue eyes, as well as strong, square jawlines.\nThey are known for their mercurial tempers, and their words are "Ours is the Fury".');
+}
+
+
